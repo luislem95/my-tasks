dcef0a940f6a1b1084844c0d67d6291f14566f96
Luis Lemus <108568782+luislem95@users.noreply.github.com>
2023-02-28 01:03:05 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Add from './component/Add';
+import List from './component/List';
+import MenuItems from './component/MenuItems';
+import SearchBar from './component/SearchBar';
+import SideBar from './component/SideBar';
+import mockData from './mockData.js';
+import ContextAPI from './ContextAPI';
+
+import{DragDropContext,Droppable}from "react-beautiful-dnd";
+
+
+
 
 function App() {
+  const onDragEnd=()=>{
+
+  }
+  const [data,setData]=useState(mockData);
+  console.log(data)
+  const updateListTitle=(updatedTitle,listId)=>{
+      const list=data.list[listId]
+      list.title=updatedTitle;
+      setData({
+        ...data,
+        lists:{
+          ...data.lists,
+          [listId]:list
+        }
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextAPI.Provider value={{updateListTitle}}>
+      <div className='container'>
+     <div className='searchBar'>
+      <SearchBar/>
+     </div>
+     <div className='menuItems'>
+      <MenuItems/>
+     </div>
+     <div className='add'>
+      <Add/>
+     </div>
+     <div className='sideBar'>
+      <SideBar/>
+     </div>
+     <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId='12345' type="list" direction='horizontal'>
+        {
+          (provided)=>(
+            <div className="list" ref={provided.innerRef}
+            {...provided.droppableProps}
+            >
+            {
+              data.listIds.map(listID=>{
+                const list=data.lists[listID] 
+                return  <List list={list} key={listID}/>
+              })
+            }
+            {provided.placeholder}
+          </div>
+          )
+        }
+
+      </Droppable>
+    
+      
+
+     </DragDropContext>
+
+</div>
+
+    </ContextAPI.Provider>
+
   );
 }
 
